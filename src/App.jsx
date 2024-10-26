01312e417476cf1feddef5953df277627be59d39
suresh-12-git <asuresh61278@gmail.com>
2024-10-26 08:31:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Fake_api from './fakestore/task1'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Fake_api/>
+     </> 
+  )
+}
+
+export default App
---
+++ src/fakestore/task1-1.jsx
@@ -0,0 +1,11 @@
+
+
+
+var Fake_store=(props)=>{
+    return(
+        <>
+        {props.children}
+        </>
+    )
+}
+export default Fake_store;
